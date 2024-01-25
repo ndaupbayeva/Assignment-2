e36f3af4f2629723569a0bab891727e648f6a335
Nuriya <230185@astanait.edu.kz>
2024-01-25 21:39:54 +0600

    Creating class Student

---
+++ src/Student.java
@@ -0,0 +1,30 @@
+public class Student extends Person {
+    private double gpa;
+
+    public Student(String name, String surname, double gpa){
+        super(name, surname);
+        this.gpa = gpa;
+    }
+
+    public String toString(){
+        return "Student: " + super.toString();
+    }
+
+    public double getGpa(){
+        return gpa;
+    }
+
+    public void setGpa(){
+        this.gpa = gpa;
+    }
+
+    @Override
+    public double getPaymentAmount(){
+        return (getGpa() > 2.67) ? 36660.00 : 0.00;
+    }
+
+    @Override
+    public int compareTo(Person o){
+        return Double.compare(this.getPaymentAmount(), o.getPaymentAmount());
+    }
+}
