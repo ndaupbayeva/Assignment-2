e90473c789f4061dca3238baf6243b2bde0aeeab
Nuriya <230185@astanait.edu.kz>
2024-01-25 22:16:16 +0600

    Adding all objects

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,9 @@ public class Main {
     public static void main(String[] args){
         ArrayList<Person> people = new ArrayList<>();
         people.add(new Employee("John", "Lennon", "Manager", 27045.78));
+        people.add(new Employee("George", "Harrison", "Developer", 50000.00));
+        people.add(new Student("Ringo", "Starr", 2.5));
+        people.add(new Student("Paul", "McCartney", 3.0));
         Collections.sort(people);
         printData(people);
     }
